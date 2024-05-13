7a7af320b455cf006ac1ccbb21539b4668ce4365
sooooraj <sooraj2536@gmail.com>
2024-05-14 02:21:34 +0530

    key

--- website/website/settings.py
+++ website/website/settings.py
@@ -24,7 +24,7 @@ load_dotenv()
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = 'django-insecure-rs_ayqydm#wz+5t8rzgeht#scen2vocl5a%+a6orhu&u8+ty)a'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
